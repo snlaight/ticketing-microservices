3b48df38231553afb125cd75c73bc98848df964f
snlaight <snlaight10@gmail.com>
2022-04-23 01:23:04 +0200

    feat: add test script

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,50 +1,45 @@
-import {MongoMemoryServer} from "mongodb-memory-server";
-import mongoose from "mongoose";
-import {app} from "../app";
-import request from "supertest";
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+import request from 'supertest';
 
 declare global {
-    var signin : () => Promise < string[] >;
+  var signin: () => Promise<string[]>;
 }
 
-let mongo : any;
+let mongo: any;
 
-beforeAll(async() => {
-    process.env.JWT_KEY = "asdf";
-    mongo = await MongoMemoryServer.create();
-    const mongoUri = mongo.getUri();
+beforeAll(async () => {
+  process.env.JWT_KEY = 'asdf';
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
 
-    await mongoose.connect(mongoUri);
+  await mongoose.connect(mongoUri);
 });
 
-beforeEach(async() => {
-    const collections = await mongoose
-        .connection
-        .db
-        .collections();
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
 
-    for (let collection of collections) {
-        await collection.deleteMany({});
-    }
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
 });
 
-afterAll(async() => {
-    await mongo.stop();
-    await mongoose
-        .connection
-        .close();
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
 });
 
-global.signin = async() => {
-    const email = "test@test.com";
-    const password = "password";
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
 
-    const response = await request(app)
-        .post("/api/users/signup")
-        .send({email, password})
-        .expect(201);
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201);
 
-    const cookie = response.get("Set-Cookie");
+  const cookie = response.get('Set-Cookie');
 
-    return cookie;
+  return cookie;
 };
