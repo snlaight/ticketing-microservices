a404b1aa38bbc82ab35444efa0a9bd193b49f66e
snlaight <snlaight10@gmail.com>
2022-05-01 01:25:13 +0200

    startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-    console.log('starting up....')
+  console.log('starting up....');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
@@ -10,25 +10,16 @@ const start = async () => {
     throw new Error('MONGO_URI must be defined');
   }
 
-  const start = async () => {
-    if (!process.env.JWT_KEY) {
-      throw new Error('JWT_KEY must be defined');
-    }
-    if (!process.env.MONGO_URI) {
-      throw new Error('MONGO_URI must be defined');
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('connected to mongodb');
+  } catch (err) {
+    console.log(err);
+  }
 
-    try {
-      await mongoose.connect(process.env.MONGO_URI);
-      console.log('connected to mongodb');
-    } catch (err) {
-      console.log(err);
-    }
-
-    app.listen(3000, () => {
-      console.log('Server is running on port 3000');
-    });
-  };
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
 };
 
 start();
