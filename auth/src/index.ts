8c7f80e38b43ca87efce665c749d09221b1aaea4
snlaight <snlaight10@gmail.com>
2022-05-01 01:21:45 +0200

    index fix for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,33 @@
-import mongoose from "mongoose";
-import {app} from "./app";
-
+import mongoose from 'mongoose';
+import { app } from './app';
 
 const start = async () => {
-  
-  
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
-=======
-const start = async() => {
+
+  const start = async () => {
     if (!process.env.JWT_KEY) {
-        throw new Error("JWT_KEY must be defined");
+      throw new Error('JWT_KEY must be defined');
     }
     if (!process.env.MONGO_URI) {
-        throw new Error("MONGO_URI must be defined");
+      throw new Error('MONGO_URI must be defined');
     }
 
-
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("connected to mongodb");
+      await mongoose.connect(process.env.MONGO_URI);
+      console.log('connected to mongodb');
     } catch (err) {
-        console.log(err);
+      console.log(err);
     }
 
     app.listen(3000, () => {
-        console.log("Server is running on port 3000");
+      console.log('Server is running on port 3000');
     });
+  };
 };
 
 start();
