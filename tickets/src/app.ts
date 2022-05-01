0ac8a598028630c6406cac4c63a14ee89f12b921
snlaight <snlaight10@gmail.com>
2022-05-01 11:58:45 +0200

    update hostname

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,20 +1,22 @@
-import express from "express";
-import "express-async-errors";
-import {json} from "body-parser";
-import cookieSession from "cookie-session";
-import {createTicketRouter} from "./routes/new";
-import {showTicketRouter} from "./routes/show";
-import {indexTicketRouter} from "./routes/index";
-import {updateTicketRouter} from "./routes/update";
-import {errorHandler, NotFoundError, currentUser} from "@snltickets/common";
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
+import { errorHandler, NotFoundError, currentUser } from '@snltickets/common';
 
 const app = express();
-app.set("trust proxy", true);
+app.set('trust proxy', true);
 app.use(json());
-app.use(cookieSession({
+app.use(
+  cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== "test"
-}));
+    secure: false,
+  })
+);
 
 app.use(currentUser);
 app.use(createTicketRouter);
@@ -22,10 +24,10 @@ app.use(showTicketRouter);
 app.use(indexTicketRouter);
 app.use(updateTicketRouter);
 
-app.all("*", () => {
-    throw new NotFoundError();
+app.all('*', () => {
+  throw new NotFoundError();
 });
 
 app.use(errorHandler);
 
-export {app};
+export { app };
